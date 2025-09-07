f893ab3f0022470bf5dd5cb440492d3679f20fbe
leaty-ell <lisalimanova14@gmail.com>
2025-09-07 20:09:32 +0700

    Update tsk11.py

--- tsk11.py
+++ tsk11.py
@@ -1,14 +1,26 @@
 cities = input("Введите названия городов через пробел: ").split()
-winner = "Петя"  # По умолчанию победитель Петя
+
+
+winner = "Петя" # По умолчанию (Петя) победит
 
 for i in range(1, len(cities)):
-    prev_city = cities[i-1].lower()
-    current_city = cities[i].lower()
-    
-    if current_city[0] != prev_city[-1]:
-        winner = "Вася" if i % 2 == 0 else "Петя"
+    previous_city = cities[i - 1].lower() 
+    current_city = cities[i].lower()       
+
+
+    if current_city[0] != previous_city[-1]:
+        # Если правило нарушено, выигрывает другой игрок
+        # Петя ходит первым (нечётные ходы), Вася ходит вторым (чётные ходы)
+        if i % 2 == 0:
+            winner = "Вася"
+        else:
+            winner = "Петя"
         break
 else:
-    winner = "Вася" if len(cities) % 2 == 0 else "Петя"
+    # Если правила соблюдаются, определяем победителя по количеству ходов
+    if len(cities) % 2 == 0:
+        winner = "Вася"
+    else:
+        winner = "Петя"
 
-print("Победитель:", winner)
+print(winner)
