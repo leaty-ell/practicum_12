e799b253e596bee1acf1280b81124416daad575f
leaty-ell <lisalimanova14@gmail.com>
2025-09-07 19:41:01 +0700

    Update tsk7.py

--- tsk7.py
+++ tsk7.py
@@ -1,4 +1,7 @@
 sentence = input()
-words = sentence.split()  
-shortest_length = min(map(len, words)) 
-print("Длина самого короткого слова:", shortest_length)
+
+def shortst_length(sentence):
+    words = sentence.split()
+    return min(len(word) for word in words)
+  
+print(shortst_length(sentence))
