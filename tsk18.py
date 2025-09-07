7ffbcf02820d05c6ff5d62a917342b5426527669
leaty-ell <lisalimanova14@gmail.com>
2025-09-07 20:46:02 +0700

    Update tsk18.py

--- tsk18.py
+++ tsk18.py
@@ -1,34 +1,25 @@
-def justify(text, width):
-    words = text.split()
-    lines = []
-    current_line = []
-    current_length = 0
-    
-    for word in words:
-        if current_length + len(word) + len(current_line) <= width:
-            current_line.append(word)
-            current_length += len(word)
-        else:
-            lines.append(current_line)
-            current_line = [word]
-            current_length = len(word)
-    if current_line:
-        lines.append(current_line)
-    
-    for i in range(len(lines)):
-        line = lines[i]
-        if len(line) == 1:
-            print(line[0])
-            continue
-        total_spaces = width - sum(len(word) for word in line)
-        gaps = len(line) - 1
-        spaces_per_gap = total_spaces // gaps
-        extra = total_spaces % gaps
-        
-        for j in range(gaps):
-            line[j] += ' ' * (spaces_per_gap + (1 if j < extra else 0))
-        print(''.join(line))
-
 text = input("Введите текст: ")
 width = int(input("Введите ширину колонки: "))
-print(format_text(text, width))
+
+words = text.split()
+line = []
+
+for word in words:
+    # Проверяем, умещается ли слово в текущую строку
+    if sum(len(w) for w in line) + len(line) + len(word) > width:
+        # Выравнивание по ширине
+        if len(line) == 1:
+            print(line[0])
+        else:
+            total_spaces = width - sum(len(w) for w in line)
+            space_between = total_spaces // (len(line) - 1)
+            extra = total_spaces % (len(line) - 1)
+            for i in range(len(line)-1):
+                print(line[i], end=" " * (space_between + (1 if i < extra else 0)))
+            print(line[-1])
+        line = [word]
+    else:
+        line.append(word)
+
+# Последняя строка
+print(" ".join(line))
