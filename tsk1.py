9c7b07a3bb144cbd737519b2543b3d8d78a95812
leaty-ell <lisalimanova14@gmail.com>
2025-09-07 19:08:52 +0700

    Update tsk1.py

--- tsk1.py
+++ tsk1.py
@@ -2,12 +2,12 @@ text = input()
 max_spaces = 0
 current = 0
 
-for char in text:
-    if char == ' ':
+for i in text:
+    if i == ' ':
         current += 1
         if current > max_spaces:
             max_spaces = current
     else:
         current = 0
-
-print("Максимальное количество последовательных пробелов:", max_spaces)
+        
+print(max_spaces)
