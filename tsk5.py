f7938079af9c5b012e04cf303c9e2dc64ce7b7e7
leaty-ell <lisalimanova14@gmail.com>
2025-09-07 19:31:24 +0700

    Update tsk5.py

--- tsk5.py
+++ tsk5.py
@@ -1,20 +1,11 @@
-s1 = input("Введите первую строку: ")
-s2 = input("Введите вторую строку: ")
-s3 = input("Введите третью строку: ")
+s1 = input()
+s2 = input()
+s3 = input()
 
-unique_chars = set()
-
-all_chars = set(s1 + s2 + s3)
-
-for char in all_chars:
-    count = 0
-    if char in s1:
-        count += 1
-    if char in s2:
-        count += 1
-    if char in s3:
-        count += 1
+unique_chars = []
+for ch in set(s1 + s2 + s3):
+    count = (ch in s1) + (ch in s2) + (ch in s3)
     if count == 1:
-        unique_chars.add(char)
+        unique_chars.append(ch)
 
-print("Символы, встречающиеся только в одной строке:", ', '.join(unique_chars))
+print(", ".join(unique_chars))
