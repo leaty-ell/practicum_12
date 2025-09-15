4b869eb102f977a55c032e6c3fa3aa8288062764
leaty-ell <lisalimanova14@gmail.com>
2025-09-15 22:47:14 +0700

    Update tsk12.py

--- tsk12.py
+++ tsk12.py
@@ -1,10 +1,11 @@
-import keyword  # модуль с ключевыми словами
+import keyword #модуль с ключевыми словами
 
-name = input()
+name = input().strip()  # убираем пробелы по краям
 
-if (not keyword.iskeyword(name)      
-    and name[0].isalpha()           # проверка, что буква
-    and name.replace("_", "").isalnum()):  # Остальные символы буквы/цифры
+if (name  # строка не пустая
+    and not keyword.iskeyword(name)  
+    and (name[0].isalpha() or name[0] == '_') 
+    and name.replace('_', '').isalnum()):  
     print("Допустимое имя")
 else:
     print("Недопустимое имя")
