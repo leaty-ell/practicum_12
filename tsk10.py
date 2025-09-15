70b96b19dc7892b5935d0f50a01dbd86991c4458
leaty-ell <lisalimanova14@gmail.com>
2025-09-15 22:30:37 +0700

    Update tsk10.py

--- tsk10.py
+++ tsk10.py
@@ -1,12 +1,12 @@
-for word in words[1:]:
-    if word != first_word:
-        unique_letters = set(word)
-        if len(unique_letters) == len(word):
-            result.append(word)
+sentence = input()              
+words = sentence.split()      
+first_word = words[0]  
+result = []            
 
-sentence = input()
-words = sentence.split()
-first_word = words[0]
-result = []
+for word in words[1:]:          
+    if word != first_word:      
+        unique_letters = set(word) 
+        if len(unique_letters) == len(word):  
+            result.append(word) 
 
-print(', '.join(result))
+print(', '.join(result))    
