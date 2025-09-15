fa613e48ba836749776dc9dddf92aa3f6c699f98
leaty-ell <lisalimanova14@gmail.com>
2025-09-15 22:52:41 +0700

    Update tsk13.py

--- tsk13.py
+++ tsk13.py
@@ -1,10 +1,13 @@
-ticket = input("Введите номер билета: ")
+k = 1
 while True:
+    ticket = input("Введите номер билета: ")
+    
     if len(ticket) % 2 == 0:
         half = len(ticket) // 2
         sum1 = sum(int(d) for d in ticket[:half])
         sum2 = sum(int(d) for d in ticket[half:])
+        
         if sum1 == sum2:
-            print("Счастливый билет найден")
+            print(f"Счастливый билет найден! Его порядковый номер: {k}")
             break
-    ticket = input("Введите следующий номер билета: ")
+    k += 1  
