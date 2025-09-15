d87473f749feee5f843b62a92ef700cefe83cd7d
leaty-ell <lisalimanova14@gmail.com>
2025-09-15 22:11:32 +0700

    Update tsk6.py

--- tsk6.py
+++ tsk6.py
@@ -3,6 +3,7 @@ sentence = input()
 def reverse_words(sentence):
     words = sentence.split()
     return " ".join(words[::-1])
-  
+
+reversed_sentence = reverse_words(sentence)
 print(reversed_sentence)
 
